992636176ae8942bccb5a93660146abaa064a577
Maximiliano Barrientos Nunez <elbodysathba@gmail.com>
2025-03-24 15:25:39 +0100

    middle
    Signed-off-by: Maximiliano Barrientos Nunez <elbodysathba@gmail.com>

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 {
-// Add services to the container.
+    builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+    builder.Services.AddProblemDetails();
 
+// Add services to the container.
+    builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+    builder.Services.AddProblemDetails();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
@@ -12,12 +19,16 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 WebApplication app = builder.Build();
 {
+    app.UseExceptionHandler();
+
 // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
     {
         app.UseSwagger();
         app.UseSwaggerUI();
     }
+
+    app.MapGet("/", () => { throw new Exception(); });
 
     app.UseHttpsRedirection();
 
@@ -26,4 +37,31 @@ WebApplication app = builder.Build();
 
 public partial class Program
 {
+}
+
+public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+    : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        logger.LogError(exception, $"Unhandled exception occurred with TraceId {httpContext.TraceIdentifier}");
+
+        ProblemDetails problemDetails = new()
+        {
+            Status = StatusCodes.Status500InternalServerError,
+            Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1",
+            Title = "Server failure",
+            Instance = httpContext.Request.Path,
+            Extensions = new Dictionary<string, object?> { { "TraceId", httpContext.TraceIdentifier } }
+        };
+
+        httpContext.Response.StatusCode = problemDetails.Status.Value;
+
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
 }
